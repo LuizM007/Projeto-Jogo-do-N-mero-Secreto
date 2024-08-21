e029a6d0afa2b8b48e3f20ed0b34b1bd867548b4
LuizM007 <164022013+LuizM007@users.noreply.github.com>
2024-08-21 19:24:47 -0300

    Criado Jogo.js

---
+++ Jogo.js
@@ -0,0 +1,22 @@
+alert('Bem-vindo ao jogo do número secreto');
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}.`);
